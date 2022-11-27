ca4b3fe733a284a8130e4bf51827fa575b217c98
Erski <eero.vilkuna@gmail.com>
2022-11-27 21:58:14 +0200

    Firebase hosting ready

---
+++ .firebase/eerovilkunadotcom/hosting/_next/static/JwQHHeM-mNNeQNQP4giiB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/104-0053b61b7199ff33.js","static/css/4fb05ef5d55886d7.css","static/chunks/pages/index-944d59a22124e3e2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
